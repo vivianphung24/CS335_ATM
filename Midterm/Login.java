be1bda5eea9a18b90fd12c508034319fb173eb57
lalimarmonyah <144646068+lalimarmonyah@users.noreply.github.com>
2024-02-07 15:49:59 -0500

    Create Login.java

---
+++ Midterm/Login.java
@@ -0,0 +1,28 @@
+package ATM;
+
+import java.util.Scanner;
+
+public class Login {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Predefined user name and password
+        String username = "user1";
+        String password = "randompass";
+
+        System.out.print("Username: ");
+        String inputUsername = scanner.nextLine();
+
+        System.out.print("Password: ");
+        String inputPassword = scanner.nextLine();
+
+        if (inputUsername.equals(username) && inputPassword.equals(password)) {
+            System.out.println("Login successful");
+        } else {
+            System.out.println("Login failed. Invalid username or password");
+        }
+
+        scanner.close();
+    }
+}
