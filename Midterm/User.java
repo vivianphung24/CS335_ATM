5e9d0af3b35fdf89303b96ffdf7c9953999d97d1
lalimarmonyah <144646068+lalimarmonyah@users.noreply.github.com>
2024-02-07 15:46:54 -0500

    Create User.java

---
+++ Midterm/User.java
@@ -0,0 +1,37 @@
+package ATM;
+
+public class User {
+	String name;
+	String password;
+	double balance;
+	
+	public User(double b) {
+		// name = n;  String n, String pw as parameters once login class is integrated
+		// password = pw;
+		balance = b;
+	}
+	
+	// getters and setters; toString
+	public String getName() {
+		return name;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void setName(String n) {
+		this.name=n;
+	}
+	
+	public void setBalance(double b) {
+		this.balance=b;
+	}
+	
+	public String toString() {
+		return (this.name + ", your balance is: $" + this.balance);
+	}
+	
+	//methods
+	
+}
