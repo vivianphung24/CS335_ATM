16358e43540d5df3963ba5972a1686cecd863655
lalimarmonyah <144646068+lalimarmonyah@users.noreply.github.com>
2024-02-07 23:39:36 -0500

    Update Withdraw.java

--- Midterm/Withdraw.java
+++ Midterm/Withdraw.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 
 public abstract class Withdraw implements Transaction {
 	
-	public static void withdraw(double balance) {
+	public static void withdraw(User user) {
 		
 	    Scanner scanner = new Scanner(System.in);
 	    System.out.println("Enter amount to withdraw: ");
 	    double amount = scanner.nextDouble();
+	    double balance = user.getBalance();
 
 		if (amount > balance) {
 	        System.out.println("Insufficient funds.");
 	    } else {
 	        balance -= amount;
-	        System.out.println("Withdrawal successful. Remaining balance: $" + balance);
+	        System.out.println("Withdrawal successful. Remaining balance: $" + user.getBalance());
 	    }
 		
-		scanner.close();
 	}
 	
 }
