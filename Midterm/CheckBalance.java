6c2f0641d7c3af530573f0261a0f51eac1772fdb
lalimarmonyah <144646068+lalimarmonyah@users.noreply.github.com>
2024-02-07 23:38:12 -0500

    Update CheckBalance.java

--- Midterm/CheckBalance.java
+++ Midterm/CheckBalance.java
@@ -2,8 +2,8 @@ package ATM;
 
 public abstract class CheckBalance implements Transaction {
 	
-	public static void checkBalance(double balance) {
-		System.out.println("Your balance is: $" + balance);
+	public static void checkBalance(User user) {
+		System.out.println("Your balance is: $" + user.getBalance());
 	}
 	
 }
