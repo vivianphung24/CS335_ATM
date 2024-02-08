0bc7d9b80b0370dde7d77df47db19722ef6eaf85
lalimarmonyah <144646068+lalimarmonyah@users.noreply.github.com>
2024-02-07 23:38:54 -0500

    Update Deposit.java

--- Midterm/Deposit.java
+++ Midterm/Deposit.java
@@ -4,16 +4,15 @@ import java.util.Scanner;
 
 public abstract class Deposit implements Transaction {
 	
-	public static void deposit(double balance) {
+	public static void deposit(User user) {
 		
 	    Scanner scanner = new Scanner(System.in);
 	    System.out.println("Enter amount to deposit: ");
 	    double amount = scanner.nextDouble();
+	    double balance = user.getBalance();
 
 	    balance += amount;
 	    System.out.println("Deposit successful. Current balance: $" + balance);
 	    
-	    scanner.close();
 	}
 }
-
