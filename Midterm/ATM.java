3a0f8c5387b38ae8dd19a70b3284ed951c067e18
MaleehahA02 <144168274+MaleehahA02@users.noreply.github.com>
2024-02-09 13:21:13 -0500

    Update ATM.java

--- Midterm/ATM.java
+++ Midterm/ATM.java
@@ -5,40 +5,42 @@ import java.util.Scanner;
 public class ATM {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean quit = false;
-        
-        User user = new User(1000.0);
+    	System.out.println("Welcome to Simmons Bank");
+    	System.out.println("What would you like to do today?");
+        try (Scanner scanner = new Scanner(System.in)) {
+			boolean quit = false;
+			
+			User user = new User(1000.0);
 
-        while (!quit) {
-            System.out.println("1. Check Balance");
-            System.out.println("2. Withdraw");
-            System.out.println("3. Deposit");
-            System.out.println("4. Quit");
-            System.out.print("Enter your choice: ");
+			while (!quit) {
+			    System.out.println("1. Check Balance");
+			    System.out.println("2. Withdraw");
+			    System.out.println("3. Deposit");
+			    System.out.println("4. Quit");
+			    System.out.print("Enter your choice: ");
 
-            int choice = scanner.nextInt();
+			    int choice = scanner.nextInt();
 
-            switch (choice) {
-                case 1:
-                    CheckBalance.checkBalance(user);
-                    break;
-                case 2:
-                    Withdraw.withdraw(user);
-                    break;
-                case 3:
-                    Deposit.deposit(user);
-                    break;
-                case 4:
-                    quit = true;
-                    break;
-                default:
-                    System.out.println("Error, enter a valid choice");
-            }
+			    switch (choice) {
+			        case 1:
+			            CheckBalance.checkBalance(user);
+			            break;
+			        case 2:
+			            Withdraw.withdraw(user);
+			            break;
+			        case 3:
+			            Deposit.deposit(user);
+			            break;
+			        case 4:
+			            quit = true;
+			            break;
+			        default:
+			            System.out.println("Error, enter a valid choice");
+			    }
 
-            System.out.println();
-        }
-
+			    System.out.println();
+			}
+		}
         System.out.println("Thank you for using the ATM");
     }
 }
