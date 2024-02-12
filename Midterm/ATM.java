6d040f7bf161d4e9b948f1cffa9046788ad7e015
lalimarmonyah <144646068+lalimarmonyah@users.noreply.github.com>
2024-02-12 13:58:08 -0500

    Update ATM.java

--- Midterm/ATM.java
+++ Midterm/ATM.java
@@ -1,46 +1,52 @@
 package ATM;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class ATM {
 
     public static void main(String[] args) {
-    	System.out.println("Welcome to Simmons Bank");
-    	System.out.println("What would you like to do today?");
-        try (Scanner scanner = new Scanner(System.in)) {
-			boolean quit = false;
-			
-			User user = new User(1000.0);
+    	
+    	Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
 
-			while (!quit) {
-			    System.out.println("1. Check Balance");
-			    System.out.println("2. Withdraw");
-			    System.out.println("3. Deposit");
-			    System.out.println("4. Quit");
-			    System.out.print("Enter your choice: ");
+        User user = new User(1000.0);
 
-			    int choice = scanner.nextInt();
+        while (!quit) {
+            System.out.println("1. Check Balance");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Deposit");
+            System.out.println("4. Quit");
+            System.out.print("Enter your choice: ");
+            
+            try {
 
-			    switch (choice) {
-			        case 1:
-			            CheckBalance.checkBalance(user);
-			            break;
-			        case 2:
-			            Withdraw.withdraw(user);
-			            break;
-			        case 3:
-			            Deposit.deposit(user);
-			            break;
-			        case 4:
-			            quit = true;
-			            break;
-			        default:
-			            System.out.println("Error, enter a valid choice");
-			    }
+            	int choice = scanner.nextInt();
 
-			    System.out.println();
-			}
-		}
+            	switch (choice) {
+                	case 1:
+                		CheckBalance.checkBalance(user);
+                		break;
+                	case 2:
+                		Withdraw.withdraw(user);
+                		break;
+                	case 3:
+                		Deposit.deposit(user);
+                		break;
+                	case 4:
+                		quit = true;
+                		break;
+                	default:
+                		System.out.println("Error: Enter a valid choice");
+                
+            	}
+            } catch (InputMismatchException e) {
+            	System.out.println("Error: Invalid input. Please enter a number.");
+            	scanner.nextLine();
+            }
+
+            System.out.println();
+        }
         System.out.println("Thank you for using the ATM");
     }
 }
