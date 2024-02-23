cd39bf74f0dd561bb7df9aabef29b3e47163d89c
Vivian Phung <49759112+vivianphung24@users.noreply.github.com>
2024-02-23 12:47:24 -0500

    Update User.java

--- Final/User.java
+++ Final/User.java
@@ -3,12 +3,14 @@ package banking;
 public class User {
 	String name;
 	String password;
-	double balance;
+	double checkingBal;
+	double savingsBal; 
 	
-	public User(double b, String string) {
+	public User(double b,double s, String string) {
 		// name = n;  String n, String pw as parameters once login class is integrated
 		// password = pw;
-		balance = b;
+		checkingBal = b;
+		savingsBal = s; 
 	}
 	
 	// getters and setters; toString
@@ -16,20 +18,29 @@ public class User {
 		return name;
 	}
 	
-	public double getBalance() {
-		return balance;
+	public double getCheckBalance() {
+		return checkingBal;
+	}
+	
+	public double getSavingsBalance() { 
+		return savingsBal; 
 	}
 	
 	public void setName(String n) {
 		this.name=n;
 	}
 	
-	public void setBalance(double b) {
-		this.balance=b;
+	public void setCheckBalance(double b) {
+		this.checkingBal=b;
+	}
+	
+	public void setSavingsBalance(double s) {
+		this.savingsBal=s; 
 	}
 	
 	public String toString() {
-		return (this.name + ", your balance is: $" + this.balance);
+		return (this.name + ", your checkings balance is: $" + this.checkingBal
+				+"\n saving balance is: $"+ this.savingsBal);
 	}
 	
 	//methods
