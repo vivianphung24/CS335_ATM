2c457479d57fc5cd79de86007f3220434c67e0fb
lalimarmonyah <144646068+lalimarmonyah@users.noreply.github.com>
2024-03-13 14:25:33 -0400

    Update Deposit.java

--- Final/Deposit.java
+++ Final/Deposit.java
@@ -4,15 +4,31 @@ import java.util.Scanner;
 
 public abstract class Deposit implements Transaction {
 	
-	public static void deposit(User user) {
+	public static double deposit(User user) {
 		
 	    Scanner scanner = new Scanner(System.in);
-	    System.out.println("Enter amount to deposit: ");
-	    double amount = scanner.nextDouble();
-	    double balance = user.getBalance();
-
-	    balance += amount;
-	    System.out.println("Deposit successful. Current balance: $" + balance);
 	    
+	    System.out.println("Select an account to deposit to: ");
+		System.out.println("1. Checking Account");
+		System.out.println("2. Savings Account");
+		int choice = scanner.nextInt();
+		
+		System.out.println("Enter amount to deposit: ");
+	    double amount = scanner.nextDouble();
+		
+		if (choice == 1) {
+			double balance = user.getCheckingBalance();
+			balance += amount;
+		    System.out.println("Deposit successful. Current balance: $" + balance);
+        } else if (choice == 2) {
+        	double balance = user.getSavingsBalance();
+        	balance += amount;
+    	    System.out.println("Deposit successful. Current balance: $" + balance);
+        } else {
+            System.out.println("Error: Invalid account choice");
+        }
+	    
+		return 0;	    
+	  
 	}
 }
