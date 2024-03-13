2c4c460a600be22b44eafb409e70aab6b6b9429e
lalimarmonyah <144646068+lalimarmonyah@users.noreply.github.com>
2024-03-13 14:25:56 -0400

    Update CheckBalance.java

--- Final/CheckBalance.java
+++ Final/CheckBalance.java
@@ -1,9 +1,26 @@
 package banking;
 
+import java.util.Scanner;
+
 public abstract class CheckBalance implements Transaction {
 	
 	public static void checkBalance(User user) {
-		System.out.println("Your balance is: $" + user.getBalance());
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Which account balance would you like to check?");
+		System.out.println("1. Checking Account");
+		System.out.println("2. Savings Account");
+		int choice = sc.nextInt();
+		
+		if (choice == 1) {
+			System.out.println("Your balance is: $" + user.getCheckingBalance());
+        } else if (choice == 2) {
+        	System.out.println("Your balance is: $" + user.getSavingsBalance());
+        } else {
+            System.out.println("Error: Invalid account choice");
+            return;
+        }
 	}
 	
 }
