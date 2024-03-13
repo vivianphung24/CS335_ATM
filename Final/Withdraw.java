f11f14f481750578ecb240ba04e0d7f8a0839129
lalimarmonyah <144646068+lalimarmonyah@users.noreply.github.com>
2024-03-13 14:26:26 -0400

    Update Withdraw.java

--- Final/Withdraw.java
+++ Final/Withdraw.java
@@ -4,20 +4,44 @@ import java.util.Scanner;
 
 public abstract class Withdraw implements Transaction {
 	
-	public static void withdraw(User user) {
+	public static double withdraw(User user) {
 		
 	    Scanner scanner = new Scanner(System.in);
-	    System.out.println("Enter amount to withdraw: ");
+		
+		System.out.println("Select an account to withdraw from: ");
+		System.out.println("1. Checking Account");
+		System.out.println("2. Savings Account");
+		int choice = scanner.nextInt();
+		
+		System.out.println("Enter amount to withdraw: ");
 	    double amount = scanner.nextDouble();
-	    double balance = user.getBalance();
-
-		if (amount > balance) {
-	        System.out.println("Insufficient funds.");
-	    } else {
-	        double newBalance = balance - amount;
-	        user.setBalance(newBalance);
-	        System.out.println("Withdrawal successful. Remaining balance: $" + user.getBalance());
-	    }
+		
+		if (choice == 1) {
+			double balance = user.getCheckingBalance();
+			if (amount > balance) {
+		        System.out.println("Insufficient funds.");
+		    } else {
+		        double newBalance = balance - amount;
+		        user.setCheckingBalance(newBalance);
+		        System.out.println("Withdrawal successful. Remaining balance: $" + user.getCheckingBalance());
+		        return(amount);
+		    }
+        } else if (choice == 2) {
+        	double balance = user.getSavingsBalance();
+			if (amount > balance) {
+		        System.out.println("Insufficient funds.");
+		    } else {
+		        double newBalance = balance - amount;
+		        user.setSavingsBalance(newBalance);
+		        System.out.println("Withdrawal successful. Remaining balance: $" + user.getSavingsBalance());
+		        return(amount);
+		    }
+        } else {
+            System.out.println("Error: Invalid account choice");
+            // return;
+        }
+	    
+		return 0;
 		
 	}
 	
