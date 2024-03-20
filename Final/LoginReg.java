b3808a7a3ef87c83a8d9abcf1abc7e03623fc3e8
MaleehahA02 <144168274+MaleehahA02@users.noreply.github.com>
2024-03-20 13:23:10 -0400

    Update LoginReg.java

--- Final/LoginReg.java
+++ Final/LoginReg.java
@@ -1,14 +1,20 @@
-package banking; 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Random;
+package banking;
+import java.io.*;
+import java.util.*;
 
+public class LoginReg implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String usern;
+    private String pass;
+    private int account;
 
-public class LoginReg {
+    public LoginReg2(String usern, String pass, int account) {
+        this.usern = usern;
+        this.pass = pass;
+        this.account = account;
+    }
 
-    static Map<String, ArrayList<String>> users = new HashMap<String, ArrayList<String>>();
+    static HashMap<String, String> users = new HashMap<String, String>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -25,82 +31,105 @@ public class LoginReg {
                     registerUser(scanner);
                     break;
                 case 2:
-                    loginUser(scanner);
+                    if (loginUser(scanner)) {
+                        ATM2.main(args);
+                        return;
+                    }
                     break;
                 case 3:
                     break;
                 default:
                     System.out.println("Invalid choice");
             }
+
+            try {
+                FileInputStream readData = new FileInputStream("userLogin.ser");
+                ObjectInputStream readStream = new ObjectInputStream(readData);
+
+                users = (HashMap<String, String>) readStream.readObject();
+
+                readStream.close();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
         }
         scanner.close();
     }
 
     static void registerUser(Scanner scanner) {
-    	Random random = new Random();
+        Random random = new Random();
         System.out.println("\nPlease Fill Out This Registration Form");
         System.out.println("-----------------");
-        
+
         // Get username
         System.out.print("Create a Username: ");
         String username = scanner.nextLine();
-        users.put(username, new ArrayList<String>());
-        
-        while (true) { 
-        // Get password
-        System.out.print("Create a Password: ");
-        String password = scanner.nextLine();
-        
-        // Validate password length
-        if (password.length() < 8) {
-            System.out.println("Passwords must be at least 8 characters long.");
-            continue;
-        }
-        
-        // Confirm password
-        System.out.print("Confirm Password: ");
-        String confirmPassword = scanner.nextLine();
-        
-        // Check if passwords match
-        if (!password.equals(confirmPassword)) {
-            System.out.println("Passwords do not match. Please try again.");
-           continue; 
-        }
-     // Generate random 3-digit number for the account number
-        int randomNumber = random.nextInt(900) + 100;
-        
-   
-        users.get(username).add(password);
-        users.get(username).add(Integer.toString(randomNumber)); 
-        
-        //prints out hashmap for testing purposes
-        for (String s : users.keySet()) {
-            System.out.println("Current key: " + s); //Optional for better understanding
-            for (String r : users.get(s)) {
-                System.out.println("Values: "+r.toString());
+
+        while (true) {
+            // Get password
+            System.out.print("Create a Password: ");
+            String password = scanner.nextLine();
+
+            // Validate password length
+            if (password.length() < 8) {
+                System.out.println("Passwords must be at least 8 characters long.");
+                continue;
             }
-        }
-        
-        System.out.println("\nRegistration successful.\n");
-        break; 
+
+            // Confirm password
+            System.out.print("Confirm Password: ");
+            String confirmPassword = scanner.nextLine();
+
+            // Check if passwords match
+            if (!password.equals(confirmPassword)) {
+                System.out.println("Passwords do not match. Please try again.");
+                continue;
+            }
+
+            users.put(username, password);
+
+            try {
+                FileOutputStream writeData = new FileOutputStream("userLogin.ser");
+                ObjectOutputStream writeStream = new ObjectOutputStream(writeData);
+
+                writeStream.writeObject(users);
+                writeStream.flush();
+                writeStream.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("\nRegistration successful.\n");
+            break;
         }
     }
-        
-      
 
-    static void loginUser(Scanner scanner) {
+    static boolean loginUser(Scanner scanner) {
         System.out.println("Enter your username:");
         String username = scanner.nextLine();
         System.out.println("Enter your password:");
         String password = scanner.nextLine();
+
         if (!users.containsKey(username)) {
-            System.out.println("Invalid username or password.\n");
+            System.out.println("Invalid username.\n");
         } else {
             if (users.get(username).equals(password)) {
                 System.out.println("Login successful.\n");
+                return true;
             } else {
                 System.out.println("Invalid username or password.\n");
             }
         }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "Username='" + usern + '\'' +
+                ", lastName='" + pass + '\'' +
+                ", birthYear=" + account +
+                "}\n";
     }
 }
