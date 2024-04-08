26802a1b7242a6f4d6140346bdca6d442b59d5d8
MaleehahA02 <144168274+MaleehahA02@users.noreply.github.com>
2024-04-08 13:24:16 -0400

    Update Deposit.java

--- Final/Deposit.java
+++ Final/Deposit.java
@@ -2,7 +2,7 @@ package banking;
 
 import java.util.Scanner;
 
-public abstract class Deposit implements Transaction {
+public abstract class Deposit {
 	
 	public static double deposit(User user) {
 		
@@ -21,16 +21,17 @@ public abstract class Deposit implements Transaction {
 			double newBalance = balance + amount;
 			user.setCheckingBalance(newBalance);
 		    System.out.println("Deposit successful. Current balance: $" + user.getCheckingBalance());
+		    user.addTransaction("Deposit to Checking Account: $" + amount); // Add the transaction to the transaction history
         } else if (choice == 2) {
         	double balance = user.getSavingsBalance();
         	double newBalance = balance + amount;
 			user.setSavingsBalance(newBalance);
     	    System.out.println("Deposit successful. Current balance: $" + user.getSavingsBalance());
+    	    user.addTransaction("Deposit to Savings Account: $" + amount); // Add the transaction to the transaction history
         } else {
             System.out.println("Error: Invalid account choice");
         }
 	    
-		return 0;	 	    
-	  
+		return 0;
 	}
 }
