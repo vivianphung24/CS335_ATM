16857c863c0031a4357c2eb447086559645796e6
Vivian Phung <49759112+vivianphung24@users.noreply.github.com>
2024-05-03 10:08:14 -0400

    Update CheckBalance.java

--- Final/CheckBalance.java
+++ Final/CheckBalance.java
@@ -2,17 +2,17 @@ package banking;
 
 import java.util.Scanner;
 
-public abstract class CheckBalance implements Transaction {
+public abstract class CheckBalance {
 	
 	public static void checkBalance(User user) {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Which account balance would you like to check?");
+		System.out.println("\nWhich account balance would you like to check?");
 		System.out.println("1. Checking Account");
 		System.out.println("2. Savings Account");
 		int choice = sc.nextInt();
-		
+		//sc.close(); 
 		if (choice == 1) {
 			System.out.println("Your balance is: $" + user.getCheckingBalance());
         } else if (choice == 2) {
