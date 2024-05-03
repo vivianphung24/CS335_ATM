e1fc256b5f62c1d07eca68c1a88dd9756d87f4fa
Vivian Phung <49759112+vivianphung24@users.noreply.github.com>
2024-05-03 10:10:50 -0400

    Update Withdraw.java

--- Final/Withdraw.java
+++ Final/Withdraw.java
@@ -1,5 +1,4 @@
 package banking;
-
 import java.util.Scanner;
 
 public abstract class Withdraw {
@@ -12,39 +11,56 @@ public abstract class Withdraw {
 		System.out.println("1. Checking Account");
 		System.out.println("2. Savings Account");
 		int choice = scanner.nextInt();
+		 
 		
+		switch (choice) {
+		case 1: 
+			withdrawCheckings(user); 
+			break; 
+		case 2: 
+			withdrawSavings(user); 
+			break; 
+		default:
+            System.out.println("Error: Invalid choice");
+		}
+         return 0;
+		
+		}
+	private static void withdrawCheckings(User user) {
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter amount to withdraw: ");
 	    double amount = scanner.nextDouble();
-		
-		if (choice == 1) {
-			double balance = user.getCheckingBalance();
+	    
+	    double balance = user.getCheckingBalance();
+	    
 			if (amount > balance) {
 		        System.out.println("Insufficient funds.");
 		    } else {
 		        double newBalance = balance - amount;
 		        user.setCheckingBalance(newBalance);
-		        System.out.println("Withdrawal successful. Remaining balance: $" + user.getCheckingBalance());
+		        System.out.println("Withdrawal successful. \nRemaining balance: $" + user.getCheckingBalance());
 		        user.addTransaction("Withdrawn from Checkings Account: $" + amount); 
-		        return(amount);
-		    }
-        } else if (choice == 2) {
-        	double balance = user.getSavingsBalance();
+		     	}
+        	} 
+		
+
+	private static void withdrawSavings(User user) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter amount to withdraw: ");
+	    double amount = scanner.nextDouble();
+	   
+	    double balance = user.getSavingsBalance();
+	    
 			if (amount > balance) {
 		        System.out.println("Insufficient funds.");
 		    } else {
 		        double newBalance = balance - amount;
 		        user.setSavingsBalance(newBalance);
-		        System.out.println("Withdrawal successful. Remaining balance: $" + user.getSavingsBalance());
+		        System.out.println("Withdrawal successful. \nRemaining balance: $" + user.getSavingsBalance());
 		        user.addTransaction("Withdrawn from Savings Account: $" + amount); 
-		        return(amount);
-		    }
-        } else {
-            System.out.println("Error: Invalid account choice");
-            // return;
-        }
-	    
-		return 0;
+		     	}
+        	} 
 		
-	}
 	
+
 }
