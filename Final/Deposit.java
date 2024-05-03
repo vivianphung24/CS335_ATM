cad6ba5cc937a8002cc430523c8a3943137f3772
Vivian Phung <49759112+vivianphung24@users.noreply.github.com>
2024-05-03 10:09:14 -0400

    Update Deposit.java

--- Final/Deposit.java
+++ Final/Deposit.java
@@ -1,4 +1,4 @@
-package banking;
+package banking; 
 
 import java.util.Scanner;
 
@@ -12,26 +12,42 @@ public abstract class Deposit {
 		System.out.println("1. Checking Account");
 		System.out.println("2. Savings Account");
 		int choice = scanner.nextInt();
+		//scanner.close(); 
+		switch (choice) {
+		case 1:
+            DepositFromCheckingsToSavings(user);
+            break;
+        case 2:
+            DepositFromSavingsToCheckings(user);
+            break;
+        default:
+            System.out.println("Error: Invalid choice");
+    }
+	return 0;
+}
+			
 		
-		System.out.println("Enter amount to deposit: ");
-	    double amount = scanner.nextDouble();
-		
-		if (choice == 1) {
+	private static void DepositFromCheckingsToSavings(User user) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter amount to deposit: ");
+			//scanner.close(); 
+			double amount = scanner.nextDouble();
 			double balance = user.getCheckingBalance();
 			double newBalance = balance + amount;
 			user.setCheckingBalance(newBalance);
-		    System.out.println("Deposit successful. Current balance: $" + user.getCheckingBalance());
+		    System.out.println("Deposit successful. \nCurrent balance: $" + user.getCheckingBalance());
 		    user.addTransaction("Deposit to Checking Account: $" + amount); // Add the transaction to the transaction history
-        } else if (choice == 2) {
+        } 
+	private static void DepositFromSavingsToCheckings(User user) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter amount to deposit: ");
+			//scanner.close(); 
+			double amount = scanner.nextDouble();
         	double balance = user.getSavingsBalance();
         	double newBalance = balance + amount;
 			user.setSavingsBalance(newBalance);
-    	    System.out.println("Deposit successful. Current balance: $" + user.getSavingsBalance());
+    	    System.out.println("Deposit successful. \nCurrent balance: $" + user.getSavingsBalance());
     	    user.addTransaction("Deposit to Savings Account: $" + amount); // Add the transaction to the transaction history
-        } else {
-            System.out.println("Error: Invalid account choice");
-        }
-	    
-		return 0;
+   
 	}
 }
